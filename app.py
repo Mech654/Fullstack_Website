7b2aa4b5e5b7d37af97faf508f52bd65d9c6c181
Mech654 <drahmet2007@hotmail.com>
2024-10-09 12:45:02 +0200

    asd

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def account():
 
 @app.route('/profile')
 def profile():
-    return render_template('Profile.html')
+    return render_template('profile.html')
 
 
 @app.route('/register', methods=['POST'])
