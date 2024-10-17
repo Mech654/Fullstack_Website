3ef6b7603d1531b1144b131627835c2cdd9012ec
Mech654 <drahmet2007@hotmail.com>
2024-10-17 08:13:17 +0200

    move .db file to static folder

--- app.py
+++ app.py
@@ -75,7 +75,7 @@ def download():
 # Route to serve the .db file
 @app.route('/download/x')
 def y():
-    return send_from_directory(directory='.', path='example.db', as_attachment=True, attachment_filename='CustomDatabaseName.db')
+    return send_from_directory(directory='static', path='example.db', as_attachment=True, attachment_filename='CustomDatabaseName.db')
 
 @app.route('/register', methods=['POST'])
 def register():
